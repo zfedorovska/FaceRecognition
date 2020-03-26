5a9ebc281299f9e8a3fb80338c00d84e8d8317eb
zoryana88 <zisynia@rambler.ru>
2020-03-26 19:38:52 +0200

    added public server path

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,8 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-	console.log("bbb");
-    fetch('http://localhost:3000/signin', {
+    fetch('https://boiling-dawn-41046.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
